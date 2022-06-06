e98dd32e9472e755178cf2f9e66080ebd676c83a
Nicolas Morel <nmr.morel@gmail.com>
2022-06-06 10:53:21 +0200

    Rename test app to bookmarks

--- apps/bookmarks/webpack.config.js
+++ apps/bookmarks/webpack.config.js
@@ -1,7 +1,8 @@
 const webpackConfig = require('@nimo/config-webpack/web')
+
 module.exports = webpackConfig({
   baseDir: __dirname,
-  name: 'test',
-  port: 8080,
+  name: 'bookmarks',
+  port: 8090,
   publicPath: '/',
 })
